13dcb8cbe8bec7e90ef4afc686c1609b720254ea
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2020-12-20 11:15:29 +0800

    Updated auth

--- public/scripts/firebase-init.js
+++ public/scripts/firebase-init.js
@@ -1,6 +1,6 @@
 var firebaseConfig = {
     apiKey: "AIzaSyAp2x4GhQ6t8SPkOMKc_pBbhjxAAGCxgOI",
-    authDomain: "feli-page.firebaseapp.com",
+    authDomain: "auth.feli.page",
     databaseURL: "https://feli-page.firebaseio.com",
     projectId: "feli-page",
     storageBucket: "feli-page.appspot.com",
