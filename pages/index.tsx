4db3399da8b55d8fd04225881217587301f29de3
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2022-04-07 13:53:57 +0100

    added auth0

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,25 @@
+import { useUser } from "@auth0/nextjs-auth0";
 import { NextPage } from "next";
 import React from "react";
+import { auth0 } from "utils/auth0";
 
 const HomePage: NextPage = () => {
-    return <div>HomePage</div>;
+    const { user } = useUser();
+    return (
+        <div>
+            <h1>HomePage</h1>
+            <a className="btn btn-sm" href={auth0.urls.login}>
+                Login
+            </a>
+            <a className="btn btn-sm" href={auth0.urls.logout}>
+                Logout
+            </a>
+
+            <code>
+                <pre>{JSON.stringify(user, null, 2)}</pre>
+            </code>
+        </div>
+    );
 };
 
 export default HomePage;
---
+++ utils/auth0.ts
@@ -0,0 +1,8 @@
+export const auth0 = {
+    urls: {
+        login: "/api/auth/login",
+        logout: "/api/auth/logout",
+        callback: "/api/auth/callback",
+        me: "/api/auth/me",
+    },
+};
